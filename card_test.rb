2e9596635feb8ae4c31208881f39a18ffeb9a6c6
Phil <Philip.Shaughnessy@gmail.com>
2017-02-08 13:15:41 -0500

    added initial requires and empty test methods

---
+++ card_test.rb
@@ -0,0 +1,30 @@
+require "./card.rb"
+require 'minitest/autorun'
+
+class CardTest < MiniTest::Test
+
+  def test_attributes_exist
+
+  end
+
+  def test_ace_value_14
+
+  end
+
+  def test_king_value_13
+
+  end
+
+  def test_queen_value_12
+
+  end
+
+  def test_jack_value_11
+
+  end
+
+  def test_card_compare
+
+  end
+  
+end
---
+++ deck_test.rb
@@ -0,0 +1,25 @@
+require "./deck.rb"
+require 'minitest/autorun'
+
+class DeckTest < MiniTest::Test
+
+  def test_52_cards
+
+  end
+
+  def test_13_of_each_suit
+
+  end
+
+  def test_four_each_face
+
+  end
+
+  def test_shuffled
+
+  end
+
+  def test_draw_card
+
+  end
+end
